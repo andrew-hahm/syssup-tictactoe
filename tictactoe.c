0133c4f3a58b7201ac02dd9fc6c929e25138a6f5
Andrew Hahm <hahm.andrew@gmail.com>
2012-05-21 14:22:48 -0400

    v0.01, added binary

--- tictactoe.c
+++ tictactoe.c
@@ -33,11 +33,13 @@ int checkwin(int board[3][3]) {
             return 1;
         }
     }
+
     if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1) {
         return 0;
     } else if (board[0][0] == 2 && board[1][1] == 2 && board[2][2] == 2) {
         return 1;
     }
+
     if (board[0][2] == 1 && board[1][1] == 1 && board[2][0] == 1) {
         return 0;
     } else if (board[0][2] == 2 && board[1][1] == 2 && board[2][0] == 2) {
